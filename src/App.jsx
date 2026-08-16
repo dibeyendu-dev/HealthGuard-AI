528ca237674cd400a5c679ad628a1c3ec11ab1e9
Dibeyendu Maity <dibeyendumaity@gmail.com>
2026-08-16 21:21:15 +0530

    Navbar almost Completeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -9,14 +12,18 @@ import History from "./pages/History";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/history" element={<History />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
